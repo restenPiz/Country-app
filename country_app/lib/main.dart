1fe2e4b3dedd18fe1e10503e544ef8e42972b3d3
restenPiz <mauropeniel7@gmail.com>
2023-08-29 23:32:18 +0200

    the datas of the api, are now available in my app

--- country_app/lib/main.dart
+++ country_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(const MainApp());
@@ -8,8 +9,23 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  //!Inicio do metodo responsavel por fazer o fetch dos dados
+  Future<void> fetchData() async {
+    final response = await http.get(Uri.parse('https://restcountries.com/v3.1/independent?status=true'));
+    if (response.statusCode == 200) {
+      final responseData = json.decode(response.body);
+      print('Dado do JSON: $responseData');
+    } else {
+      // Lidar com erros
+      print('Erro na requisição: ${response.statusCode}');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    fetchData();
+    
     return MaterialApp(
 
       //?Removendo o debugger do meu app
