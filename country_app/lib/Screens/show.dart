dc022257aac960631855ffa880c5c1635d289987
restenPiz <mauropeniel7@gmail.com>
2023-08-30 00:55:39 +0200

    Add another screen to my app to show the details of any country

---
+++ country_app/lib/Screens/show.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class showScreen extends StatefulWidget {
+  const showScreen({super.key});
+
+  @override
+  State<showScreen> createState() => _showScreenState();
+}
+
+class _showScreenState extends State<showScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
